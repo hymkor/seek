e87b73d755c6e70dc292e55cf45d68cccc0402b9
Kaoru HAYAMA <iyahaya@nifty.com>
2017-11-11 18:08:08 +0900

    make argf's sample-grep to color-grep

--- argf/grep/grep.go
+++ argf/grep/grep.go
@@ -3,9 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
-	"github.com/zetamatta/experimental/argf"
 	"os"
 	"regexp"
+
+	"github.com/mattn/go-colorable"
+	"github.com/zetamatta/experimental/argf"
 )
 
 func main1() error {
@@ -16,10 +18,21 @@ func main1() error {
 	if err != nil {
 		return err
 	}
+	out := colorable.NewColorableStdout()
 	r := argf.NewFiles(os.Args[2:])
 	for r.Scan() {
-		if rx.MatchString(r.Text()) {
-			fmt.Printf("%s(%d): %s\n", r.Filename(), r.FNR(), r.Text())
+		text := r.Text()
+		m := rx.FindAllStringIndex(text, -1)
+		if m != nil {
+			fmt.Fprintf(out, "\x1B[35;1m%s:\x1B[32;1m%d\x1B[36;1m:\x1B[37;1m", r.Filename(), r.FNR())
+			last := 0
+			for i := 0; i < len(m); i++ {
+				fmt.Fprintf(out, "%s\x1B[31;1m%s\x1B[37;1m",
+					text[last:m[i][0]],
+					text[m[i][0]:m[i][1]])
+				last = m[i][1]
+			}
+			fmt.Fprintln(out, text[last:])
 		}
 	}
 	return r.Err()
